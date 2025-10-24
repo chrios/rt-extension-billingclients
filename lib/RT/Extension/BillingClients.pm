dd19ee25ec2442745db75d935192d61c94cf4cb5
Christopher Frew <christopher.frew@mawsontech.com.au>
2025-10-24 22:43:36 +0930

    add ability to edit trransaction custom fields

---
+++ lib/RT/Extension/BillingClients.pm
@@ -0,0 +1,29 @@
+
+package RT::Extension::BillingClients;
+
+use strict;
+use warnings;
+
+our $VERSION = '0.02';
+
+1;
+
+__END__
+
+=head1 NAME
+
+RT::Extension::BillingClients - Sync and edit billing-related custom fields
+
+=head1 DESCRIPTION
+
+Adds GUI for editing transaction custom fields.
+
+=head1 INSTALLATION
+
+Standard RT extension installation steps.
+
+=head1 AUTHOR
+
+Christopher Frew
+
+=cut
